6b89ad4f7d30718e5d43bc119567c10e1f8445da
rodims-code <ridrodims@hotmail.com>
2024-12-12 06:17:35 +0100

    Premier mis en oeuvre du js

---
+++ index.js
@@ -0,0 +1,22 @@
+// alert("C'est comment là bas !");
+// console.log();
+
+//Kamel Casse
+let maSuperVariable = "Hello";
+
+let ma_variable = "Hello world";
+console.log(ma_variable);
+
+// ** Les variables **
+var untext = 'this is a text'
+ 
+const prenom = "Julie";
+
+console.log(prenom);
+
+let unChiffre = 24;
+
+console.log(unChiffre);
+
+unChiffre = 22;
+
