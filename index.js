8c5af40ce4e4c54f1f09c3af345e1a3727826815
rodims-code <ridrodims@hotmail.com>
2024-12-17 13:27:38 +0100

    Les fonctions

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 /* https://developer.mozilla.org/en-US/docs/Web/JavaScript */
+/* https://devdocs.io/*/
 
-/* 
-premier boucle
-let i = 0;
-while(i < 10){
-    console.log('bonjour' + i)
-    i = i + 1
+
+
+/* function canDrive(age, pays){
+    if(
+        (age >= 18 && pays === 'FR') ||
+        (age >= 16 && pays === 'US')
+    ){
+        return true
+    }
+    return false
+}
+
+console.log(canDrive(17, 'US')) */
+
+// Exercice
+// On cree un nombre entre 0 et 20 
+// 3 essaies pour deviner le mot 
+// isRight(n)
+// guess()
+
+/* function getRandomInt(max){
+    return Math.floor(Math.random() * (max + 1))
+}
+const solution = getRandomInt(10)
+console.log(solution)
+
+function isRight(n){
+    return solution === n
+} 
+
+function guess(){
+    const number = prompt('Entrez un chffre') * 1
+    return isRight(number)
+}
+
+for (i = 0; i < 3; i++){
+    if (guess()){
+        console.log('Bravo')
+        break;
+    } else if (i === 2){
+        console.log('Vous avez perdu')
+    }
 } */
 
-const notes = [12, 14, 10]
-
-for (let i in notes){
-    console.log(i)
-}
