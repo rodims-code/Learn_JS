937030bb0ce3a8e2235fc2954865309812297f56
rodims-code <ridrodims@hotmail.com>
2024-12-26 05:54:30 +0100

    Les class

--- index.js
+++ index.js
@@ -35,82 +35,36 @@ const moyenne = (notes) =>{
 } */
 
 
-const canDrive = function (age, pays){
-    if(
-        (age > 18 && pays === 'FR') ||
-        (age > 16 && pays === 'US')
-    ){
-        return true
+
+const moyenne = (notes) => {
+    let sum = 0
+    for (let note of notes){
+        sum = sum + note
     }
-    return false
+    return sum / notes.length
 }
-console.log(canDrive(17, 'US'))
+class Student{
+    ecole = 'ligth word school'
+    constructor(firstname, lastname){
+        this.firstname = firstname
+        this.lastname = lastname
+    }
 
-function greeting(name){
-    console.log(`bonjour ${name}`)
-}
+    setNotes (notes){
+        this.notes = notes
+    }
 
-greeting('jhon')
-
-function maFunction(){
-    console.log(this)
-}
-maFunction.call(3)
-
-const a = {
-    firstname : 'john',
-    lastname : 'Deo',
-    fullname : function (){
-        console.log(this)
-        console.log(`${this.firstname} ${this.lastname}`)
+    canPass (){
+        return moyenne(this.notes) >= 10
     }
 }
 
-a.fullname()
-
-const somme = (a, b) =>{
-    return a + b
-}
-console.log(somme(1, 2))
-
-isPaire = function (a, cb) {
-    if(a % 2 === 0){
-        cb(a)
-    }
-}
-
-isPaire(6, function (n) {
-    console.log(`Mon nombre ${n} est paire `)
-})
-
-//Exo
-/* 
-On cree un monbre entre 0 & 10
-3 eessaies pour deviner le mot 
-isRight(n)
-guess()
- */
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-const solution = getRandomInt(10)
-console.log(solution)
-
-function isRigth(n){
-    return solution === n 
-}
-
-function guess(){
-    const number = prompt('Entrez un chiffre')
-    return isRigth(number)
-}
-
-for (i = 0; i < 3; i++){
-    if(guess()){
-        console.log('bravo')
-        break
-    }else if (i === 2){
-        console.log('vous  avez perdu !')
-    }
-}
+const john = new Student('John', 'Deo')
+const jean= new Student('Jean', 'Marc')
+john.setNotes(
+    [10, 10, 9]
+)
+jean.setNotes(
+    [15, 18, 16] 
+)
+console.log(john.canPass(), jean.canPass())
