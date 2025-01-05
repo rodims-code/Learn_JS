11f7ba6cd76567d7923d441644167315ce0e5deb
rodims-code <ridrodims@hotmail.com>
2025-01-05 21:19:02 +0100

    myArray after delay

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 
 
 
-setTimeout(() => {
-    console.log('Hello World');
-}, 10000);
+const myArray = ["zero", "one", "two"];
+myArray.myMethod = function (sProperty) {
+  console.log(arguments.length > 0 ? this[sProperty] : this);
+};
+
+myArray.myMethod(); // prints "zero,one,two"
+myArray.myMethod(1); // prints "one"
+
+setTimeout(myArray.myMethod, 1.0 * 1000); // prints "[object Window]" after 1 second
+setTimeout(myArray.myMethod, 1.5 * 1000, "1"); // prints "undefined" after 1.5 seconds
+setTimeout(myArray.myMethod, 2.0 * 1000, "1"); // prints "undefined" after 2 seconds
